f5a1d2896ea6a17f8a7e810177e131fdc559e7c2
Sam Backus <Samuel.Backus@gmail.com>
2014-05-09 19:04:38 -0400

    add flash message when validation fails

--- app/views.py
+++ app/views.py
@@ -21,9 +21,12 @@ def index():
         user = user,
         posts = posts)
 
-@app.route('/ligin', methods = ['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for OpenID="'+ form.openid.data + '", remember_me=' + str(form.remember_me.data))
+    return redirect('/index')
   return render_template('login.html',
     title = 'Sign In',
     form = form)
